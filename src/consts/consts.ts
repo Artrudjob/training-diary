76395f95216b02ab58aab89739d8ea6eed1412ab
Artrudjob <Artrudjob@yandex.ru>
2022-09-07 18:02:20 +0300

    feat: add consts

---
+++ src/consts/consts.ts
@@ -0,0 +1,46 @@
+export const citationSchwarzenegger = [
+    {
+        citation: "There are no shortcuts—everything is reps, reps, reps.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Just like in bodybuilding, failure is also a necessary experience for growth in our own lives, for if" +
+            " we're never tested to our limits, how will we know how strong we really are? How will we ever grow?",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Positive thinking can be contagious. Being surrounded by winners helps you develop into a winner.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Stop whining.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "I play the Terminator, but you guys are the true terminators.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Just remember, you can't climb the ladder of success with your hands in your pockets.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Going through the pain barrier, that's what makes the muscles grow. There's this pain, this aching, " +
+            "and going on and on and on. That's what divides a champion from a non-champion - having the guts to go through the pain barrier.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "In sports, you learn competition means go after it, win it. And not to let anything else interfere. " +
+            "It's something you learn, and it sticks with you. I consider myself an expert in looking into a particular idea or goal and then going after it without anything else in mind… It's always the same kind of thing. You pick a goal, and then you just go after it, accomplish it, and get satisfaction out of that.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "Strength does not come from winning. Your struggles develop your strengths. When you go through " +
+            "hardships and decide not to surrender, that is strength.",
+        author: "Arnold Schwarzenegger"
+    },
+    {
+        citation: "You have to see yourself winning before you win.",
+        author: "Arnold Schwarzenegger"
+    },
+]
