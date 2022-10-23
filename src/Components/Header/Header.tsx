5427befe84e4666847d15f1571f2d6c1b2725cd6
Artrudjob <Artrudjob@yandex.ru>
2022-10-23 13:49:19 +0300

    feat: add scroll effect for title

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {FC} from "react";
 import styles from "./header.module.css";
 
-class Header extends React.Component<any, any> {
-    render() {
-        return (
-            <header className={styles.header}>
-                    <h1 className={styles.header__title}>MAKE YOURSELF</h1>
-            </header>
-        );
-    }
+type TProps = {
+    miniHeader: boolean
+}
+
+const Header: FC<TProps> = ({miniHeader}): JSX.Element => {
+
+    return (
+        <header className={styles.header}>
+            <h1 className={miniHeader? styles.header__title_mini : styles.header__title}>MAKE YOURSELF</h1>
+        </header>
+    );
+
 };
 
 export default Header;
--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, {FC, useEffect, useState} from "react";
 import styles from "./layout.module.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 
-class Layout extends React.Component<any, any> {
+const Layout: FC = (): JSX.Element => {
+    const [windowPosY, setWindowPosY] = useState<number>(0);
+    const [miniHeader, setMiniHeader] = useState<boolean>(false);
 
-    render() {
-        return (
-            <>
-                <Header />
-                <main className={styles.main}>
-                    <Outlet />
-                </main>
-                <Footer />
-            </>
-        )
-    }
+    useEffect(() => {
+        function handleScroll() {
+            setWindowPosY(window.scrollY);
+
+            (windowPosY < 110) ? setMiniHeader(false) : setMiniHeader(true);
+        }
+        console.log(windowPosY);
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    })
+
+    return (
+        <>
+            <Header miniHeader={miniHeader}/>
+            <main className={styles.main}>
+                <Outlet/>
+            </main>
+            <Footer/>
+        </>
+    )
 }
 
 export default Layout;
